d9d755cf830ff0285c995e5bbf03e0d7f385ba9a
Travis CI User <travis@example.org>
2019-07-23 18:21:50 +0000

    Documentation for bonifaido/rust-zookeeper@f4a47e7

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["mio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mio/struct.Registration.html\" title=\"struct mio::Registration\">Registration</a>",synthetic:false,types:["mio::poll::Registration"]},];
+implementors["syn"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"syn/parse/struct.ParseBuffer.html\" title=\"struct syn::parse::ParseBuffer\">ParseBuffer</a>&lt;'a&gt;",synthetic:false,types:["syn::parse::ParseBuffer"]},];
+implementors["zookeeper"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"zookeeper/struct.ZooKeeper.html\" title=\"struct zookeeper::ZooKeeper\">ZooKeeper</a>",synthetic:false,types:["zookeeper::zookeeper::ZooKeeper"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
